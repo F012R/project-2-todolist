ff4cdb5e0eacc77df0132523b95ed75f6f3d6cba
Jang Eunsun <20211234@duksung.ac.kr>
2025-02-05 00:00:04 +0900

    ✨ Feat: 할 일 등록 기능 추가

---
+++ todolist/src/main/java/kr/or/f012r/todo/TodoAddServlet.java
@@ -0,0 +1,35 @@
+package kr.or.f012r.todo;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/addTodo")
+public class TodoAddServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private TodoDao todoDao = new TodoDao();
+       
+    public TodoAddServlet() {
+        super();
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        
+        String title = request.getParameter("title");
+        String name = request.getParameter("name");
+        int sequence = Integer.parseInt(request.getParameter("sequence"));
+
+        int result = todoDao.addTodo(title, name, sequence);
+
+        if (result > 0) {
+            response.sendRedirect("main");
+        } else {
+            request.setAttribute("error", "할 일 추가에 실패했습니다.");
+            request.getRequestDispatcher("/addTodo.jsp").forward(request, response);
+        }
+    }
+}
